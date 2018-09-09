bf703357dd35ad30c887db061622c165dbde4f53
MikaCinc <mihajlo.ls00@outlook.com>
2018-09-09 16:42:14 +0200

    Tabs component done!

---
+++ src/Content/String/Components/Convert.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Convert extends Component {
+    render() {
+        return (
+            <div>
+                0
+            </div>
+        );
+    }
+}
+
+export default Convert;
--- src/Content/String/index.js
+++ src/Content/String/index.js
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
 
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+/* myComponents */
+import Convert from "./Components/Convert";
+import Decorate from "./Components/Decorate";
+
 class String extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            current: 0
+        }
+    }
+
+    handleChange(e, value) {
+        this.setState({
+            current: value
+        })
+    }
+
+    renderTabs() {
+        return (
+            <Paper square style={{ width: 500, marginLeft: "auto", marginRight: "auto" }}>
+                <Tabs
+                    value={this.state.current}
+                    onChange={this.handleChange}
+                    fullWidth
+                    indicatorColor="secondary"
+                    textColor="secondary"
+                >
+                    <Tab
+                        icon={
+                            <i className="material-icons">autorenew</i>
+                        }
+                        label="CONVERT"
+                    />
+                    <Tab
+                        icon={
+                            <i className="material-icons">text_format</i>
+                        }
+                        label="DECORATE"
+                    />
+                    <Tab
+                        icon={
+                            <i className="material-icons">text_fields</i>
+                        }
+                        label="STYLE"
+                        disabled
+                    />
+                </Tabs>
+            </Paper>
+        );
+    }
+
+    renderTabContent() {
+        switch(this.state.current) {
+            case 0: return <Convert/>;
+            case 1: return <Decorate/>;
+            default: return <Convert/>;
+        }
+    }
+
     render() {
         return (
             <div>
-                
+                {this.renderTabs()}
+                {this.renderTabContent()}
             </div>
         );
     }
