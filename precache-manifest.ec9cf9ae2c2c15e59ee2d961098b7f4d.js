d144cbc6d6f74769dfe2f349575971f5d32b9bfd
MikaCinc <mihajlo.ls00@outlook.com>
2019-09-01 15:51:27 +0200

    Updates

---
+++ precache-manifest.ec9cf9ae2c2c15e59ee2d961098b7f4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a15ea92c6b8d7be02db9d50b8ce35f",
+    "url": "/Neon-React/index.html"
+  },
+  {
+    "revision": "8fd01cc6388f3a2825a2",
+    "url": "/Neon-React/static/css/main.38c48249.chunk.css"
+  },
+  {
+    "revision": "9887b681eab252441574",
+    "url": "/Neon-React/static/js/2.fde4114e.chunk.js"
+  },
+  {
+    "revision": "8fd01cc6388f3a2825a2",
+    "url": "/Neon-React/static/js/main.878b1037.chunk.js"
+  },
+  {
+    "revision": "7439d52868899863b29a",
+    "url": "/Neon-React/static/js/runtime~main.30e47038.js"
+  },
+  {
+    "revision": "9577f6bf14f9e8bcf1835b254256c6d1",
+    "url": "/Neon-React/static/media/avatar.9577f6bf.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Neon-React/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Neon-React/precache-manifest.d247b857edd5c427ebbcc8dd5b8cfd4b.js"
+  "/Neon-React/precache-manifest.ec9cf9ae2c2c15e59ee2d961098b7f4d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Neon-React/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Neon-React/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
